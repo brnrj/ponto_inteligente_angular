5f8649384a0fc08792382daf89fc845a25a51bc4
Bruno Martins <bruno.martins217@gmail.com>
2021-12-14 22:19:00 -0300

    criando o servico de login

--- src/app/autenticacao/login/components/login/login.component.ts
+++ src/app/autenticacao/login/components/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { Login } from '../../models';
+import { LoginService } from '../../services';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,7 @@ import { Login } from '../../models';
 export class LoginComponent implements OnInit {
 
   form: FormGroup;
-  constructor(private fb: FormBuilder, private snackBar: MatSnackBar, /*private router: Router*/) { }
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar, private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
     this.gerarForm();
@@ -33,6 +34,30 @@ export class LoginComponent implements OnInit {
       return;
     }
     const login: Login = this.form.value
-    alert('Email: ' + login.email + ' senha: ' + login.senha);
+    this.loginService.logar(login)
+      .subscribe(
+        data => {
+          console.log(JSON.stringify(data));
+          localStorage['token'] = data['data']['token'];
+          const usuarioData = JSON.parse(
+            atob(data['data']['token'].split('.')[1]));
+          console.log(JSON.stringify(usuarioData));
+          if (usuarioData['role'] == 'ROLE_ADMIN') {
+            alert('Direcionar para página de admin');
+            //this.router.navigate(['/admin']);
+          } else {
+            alert('Deve redirecionar para a página de funcionário');
+            //this.router.navigate(['/funcionario']);
+          }
+        },
+        err => {
+          console.log(JSON.stringify(err));
+          let msg: string = "Tente novamente";
+          if(err['status'] === 401) {
+            msg = "Email/senha inválido(s)."
+          }
+          this.snackBar.open(msg, "Erro", { duration: 5000 })
+        }
+      )
   }
 }
